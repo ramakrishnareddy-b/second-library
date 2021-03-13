0b03a1096758782d7374b29764edcf6655dcd24a
Ramakrishnareddy Bandi <ram1986b@gmail.com>
2021-03-13 13:33:22 +0530

    Updates

---
+++ precache-manifest.062c319d41faba565d72066cf0059c21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2961c64aef93700d4d96d43bac736737",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a1807b2ca51d5d8f4d0b",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "875578ab093391719e46",
+    "url": "./static/js/2.c91a2f6f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c91a2f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1807b2ca51d5d8f4d0b",
+    "url": "./static/js/main.bccc6cdd.chunk.js"
+  },
+  {
+    "revision": "3d92ff5ae390737e09c1",
+    "url": "./static/js/runtime-main.ff53a9fe.js"
+  }
+]);
